14cd494f9809c2c27fde2e92e1bdec4a5d24ea83
JairoDuenas <jairoduenas.ing@gmail.com>
2023-03-23 14:21:23 -0500

    Corrigiendo error app tienda[registro admin]

--- tienda/admin.py
+++ tienda/admin.py
@@ -8,7 +8,7 @@ class Categoria_add_admin(admin.ModelAdmin):
 
 class Producto_add_admin(admin.ModelAdmin):
   readonly_fields = ('created', 'updated')
-  list_display = ('nombre')
+  list_display = ('nombre', )
 
 admin.site.register(CategoriaProducto, Categoria_add_admin)
 admin.site.register(Producto, Producto_add_admin)
