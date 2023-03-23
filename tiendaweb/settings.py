626a7d7407ec37bba191d4dbe857192506fe677a
JairoDuenas <jairoduenas.ing@gmail.com>
2023-03-23 18:08:47 -0500

    Creando app autenticacón, agregando variable global[settings]

--- tiendaweb/settings.py
+++ tiendaweb/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'blog',
     'contacto',
     'tienda',
+    'carro',
+    'autenticacion',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'carro.context_processor.importe_total_carro',
             ],
         },
     },
