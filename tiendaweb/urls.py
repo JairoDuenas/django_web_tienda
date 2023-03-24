a7a4694134eda125ee8f5f36bc923379a4a2a435
JairoDuenas <jairoduenas.ing@gmail.com>
2023-03-24 08:08:40 -0500

    Se crea app pedidos[template,views, urls, registro]

--- tiendaweb/urls.py
+++ tiendaweb/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('contacto/', include('contacto.urls')),
     path('tienda/', include('tienda.urls')),
     path('carro/', include('carro.urls')),
-    path('autenticacion/', include('autenticacion.urls'))
+    path('autenticacion/', include('autenticacion.urls')),
+    path('pedidos/', include('pedidos.urls'))
 ]
